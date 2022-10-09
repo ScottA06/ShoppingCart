04da01e28e3380c41504d6cc4ed48b10b8bd1399
ScottA06 <109874293+ScottA06@users.noreply.github.com>
2022-10-09 14:37:39 -0600

    Add files via upload

--- ShoppingCart/index.js
+++ ShoppingCart/index.js
@@ -16,34 +16,31 @@ $(document).ready(function () {
         <div class="quantity">0</div> \
         <i class="bi bi-plus-lg"></i> \
         </div> \
+        <button class="remove">Remove from List</button> \
         </div> \
       ');
     }
 
-    var sortItem = function () {
-        var prices = $('.item-amount');
-        var names = $('.item-input');
-        var priceName = [];
-        for (i = 0; i < prices.length; i++) {
-            var price = $(prices[i]).text().trim();
-            var name = $(names[i]).text().trim();
-            priceName.push([name, price]);
-        }
-        priceName.sort();
-        console.log(priceName);
-        for (i = 0; i < priceName.length; i++) {
-            $($('.item-input')[i]).text(priceName[i][0]);
-            $($('.item-amount')[i]).text(priceName[i][1]);
-        }
-    }
+    // var sortItem = function () {
+    //     var prices = $('.item-amount');
+    //     var names = $('.item-input');
+    //     var priceName = [];
+    //     for (i = 0; i < prices.length; i++) {
+    //         var price = $(prices[i]).text().trim();
+    //         var name = $(names[i]).text().trim();
+    //         priceName.push([name, price]);
+    //     }
+    //     priceName.sort();
+    //     console.log(priceName);
+    //     for (i = 0; i < priceName.length; i++) {
+    //         $($('.item-input')[i]).text(priceName[i][0]);
+    //         $($('.item-amount')[i]).text(priceName[i][1]);
+    //     }
+    // }
 
     // var removeItem = function() {
     //   this.parents('.row').remove();
     // }
-
-    $(document).on('click', '#sort', function () {
-        sortItem();
-    });
 
     $(document).on('click', '#addToList', function () {
         addItem($('#item-input').val(), $('#item-amount').val());
@@ -54,23 +51,23 @@ $(document).ready(function () {
         sum();
     });
 
-    $('input').keydown(function (e) {
+    // $('input').keydown(function (e) {
 
-        if (e.which == 13) {
-            sum();
-        };
-    });
+    //     if (e.which == 13) {
+    //         sum();
+    //     };
+    // });
 
-    $(document).on('keyup', '.quantity', function () {
-        sum();
-    });
+    // $(document).on('keyup', '.quantity', function () {
+    //     sum();
+    // });
 
-    $(document).on('keydown', '#cost', function (e) {
+    // $(document).on('keydown', '#cost', function (e) {
 
-        if (e.which == 13) {
-            addItem($('#name').val(), $('#cost').val());
-        };
-    });
+    //     if (e.which == 13) {
+    //         addItem($('#name').val(), $('#cost').val());
+    //     };
+    // });
 
 
 
